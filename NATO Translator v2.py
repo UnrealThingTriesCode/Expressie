ac0dda5678df14c23b25c7696d6aca894f18a8e5
Sunka <unrealthingtriescodedev@gmail.com>
2021-11-08 14:00:09 +0100

    completed the addition of Morse code and number]

--- NATO Translator v2.py
+++ NATO Translator v2.py
@@ -1,26 +1,34 @@
 __author__ = ['Sunka Labree']
 __date__ = ['2021.10.14 -> 2021.10.27']
-# imports sys.argv
+# Imports sys.argv
 import sys
 
 # Creates a dictionary
-dicti = {'a': 'alpha   |  *-', 'b': 'bravo   |  -***', 'c': 'charlie |  -*-*',
-         'd': 'delta   |  -**', 'e': 'echo    |  *', 'f': 'foxtrot |  **-*',
-         'g': 'golf    |  --*', 'h': 'hotel   |  ****', 'i': 'india   |  **',
-         'j': 'juliet  |  *---', 'k': 'kilo    |  -*-', 'l': 'lima    |  *-**',
-         'm': 'mike    |  --', 'n': 'november| -*', 'o': 'oscar   |  ---',
-         'p': 'papa    |  *--*', 'q': 'quebec  |  --*-', 'r': 'romeo   |  *-*',
-         's': 'sierra  |  ***', 't': 'tango   |  -', 'u': 'uniform |  **-',
-         'v': 'victor  |  ***-', 'w': 'whiskey |  *--', 'x': 'x-ray   |  -**-',
-         'y': 'yankee  |  -*--', 'z': 'zulu.   |  --**'}
+dicti = {'a': 'Alpha   |  *-', 'b': 'Bravo   |  -***', 'c': 'Charlie |  -*-*',
+         'd': 'Delta   |  -**', 'e': 'Echo    |  *', 'f': 'Foxtrot |  **-*',
+         'g': 'Golf    |  --*', 'h': 'Hotel   |  ****', 'i': 'India   |  **',
+         'j': 'Juliet  |  *---', 'k': 'Kilo    |  -*-', 'l': 'Lima    |  *-**',
+         'm': 'Mike    |  --', 'n': 'November|  -*', 'o': 'Oscar   |  ---',
+         'p': 'Papa    |  *--*', 'q': 'Quebec  |  --*-', 'r': 'Romeo   |  *-*',
+         's': 'Sierra  |  ***', 't': 'Tango   |  -', 'u': 'Uniform |  **-',
+         'v': 'Victor  |  ***-', 'w': 'Whiskey |  *--', 'x': 'X-ray   |  -**-',
+         'y': 'Yankee  |  -*--', 'z': 'Zulu.   |  --**', '1': 'One (Pronounce as Wun)    |  *----',
+          '2': 'Two (Pronounce as Too)    |  **---', '3': 'Three (Pronounce as Tree) |  ***--',
+         '4': 'Four (Pronounce as Fower  |  ****-', '5': 'Five (Pronounce as Fife)  |  *****',
+         '6': 'Six (Pronounce as Six)    |  -****', '7': 'Seven (Pronounce as Sevun)|  --***',
+         '8': 'Eight (Pronounce as Ait)  |  ---**', '9': 'Nine (Pronounce as Niner) |  ----*',
+         '0': 'Zero (Pronounce as Zeero) |  -----'}
+         
+         
+         
+         
 
 # Checks if user has input words
-if len(sys.argv) > 1:
-    user_input = sys.argv[1]
+if len(sys.argv) < -1:
+    user_input = sys.argv[-1]
 else:
-    user_input = input('Enter one or more letters to translate: ')
+    user_input = input('Enter Letters and Numbers to Translate: ')
 # Translates given letters to NATO
 for letter in user_input:
     translation = dicti.get(letter)
     print(letter, translation, sep='\t')
-         
