f1d9ad7a7943e4a97b704282f2534d7347d306d3
Sunka <unrealthingtriescodedev@gmail.com>
2021-11-08 11:57:19 +0100

    Update NATO Translator v2.py

--- NATO Translator v2.py
+++ NATO Translator v2.py
@@ -4,15 +4,15 @@ __date__ = ['2021.10.14 -> 2021.10.27']
 import sys
 
 # Creates a dictionary
-dicti = {'a': 'alpha', 'b': 'bravo', 'c': 'charlie',
-         'd': 'delta', 'e': 'echo', 'f': 'foxtrot',
-         'g': 'golf', 'h': 'hotel', 'i': 'india',
-         'j': 'juliet', 'k': 'kilo', 'l': 'lima',
-         'm': 'mike', 'n': 'november', 'o': 'oscar',
-         'p': 'papa', 'q': 'quebec', 'r': 'romeo',
-         's': 'sierra', 't': 'tango', 'u': 'uniform',
-         'v': 'victor', 'w': 'whiskey', 'x': 'x-ray',
-         'y': 'yankee', 'z': 'zulu'}
+dicti = {'a': 'alpha   |  *-', 'b': 'bravo   |  -***', 'c': 'charlie |  -*-*',
+         'd': 'delta   |  -**', 'e': 'echo    |  *', 'f': 'foxtrot |  **-*',
+         'g': 'golf    |  --*', 'h': 'hotel   |  ****', 'i': 'india   |  **',
+         'j': 'juliet  |  *---', 'k': 'kilo    |  -*-', 'l': 'lima    |  *-**',
+         'm': 'mike    |  --', 'n': 'november| -*', 'o': 'oscar   |  ---',
+         'p': 'papa    |  *--*', 'q': 'quebec  |  --*-', 'r': 'romeo   |  *-*',
+         's': 'sierra  |  ***', 't': 'tango   |  -', 'u': 'uniform |  **-',
+         'v': 'victor  |  ***-', 'w': 'whiskey |  *--', 'x': 'x-ray   |  -**-',
+         'y': 'yankee  |  -*--', 'z': 'zulu.   |  --**'}
 
 # Checks if user has input words
 if len(sys.argv) > 1:
@@ -24,3 +24,4 @@ for letter in user_input:
     translation = dicti.get(letter)
     print(letter, translation, sep='\t')
     
+
